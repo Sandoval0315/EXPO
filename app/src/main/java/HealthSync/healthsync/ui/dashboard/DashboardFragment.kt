8fce77864434831fceb60139f270171cb272f4f8
ethan <20230006@ricaldone.edu.sv>
2024-07-14 10:29:27 -0600

    termine de programar los botones

--- app/src/main/java/HealthSync/healthsync/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/HealthSync/healthsync/ui/dashboard/DashboardFragment.kt
@@ -1,6 +1,11 @@
 package HealthSync.healthsync.ui.dashboard
 
+import HealthSync.healthsync.Activity_rutinafuerza
 import HealthSync.healthsync.R
+import HealthSync.healthsync.activity_altaintensidad
+import HealthSync.healthsync.activity_rutina_cardiovascular
+import HealthSync.healthsync.activity_rutinas
+import HealthSync.healthsync.activity_yoga
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +14,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import HealthSync.healthsync.databinding.FragmentDashboardBinding
+import android.content.Intent
 import android.widget.Button
 
 class DashboardFragment : Fragment() {
@@ -40,6 +46,25 @@ class DashboardFragment : Fragment() {
         val btnRunnig = root.findViewById<Button>(R.id.btnRunnig)
         val btnAltaIntensidad = root.findViewById<Button>(R.id.btnAltaIntensidad)
         val btnYoga = root.findViewById<Button>(R.id.btnYoga)
+
+        btnRutinaCardio.setOnClickListener{
+            val intent = Intent(requireContext(), activity_rutina_cardiovascular::class.java)
+            startActivity(intent)
+        }
+        btnFuerzaPesas.setOnClickListener {
+            val intent = Intent(requireContext(), Activity_rutinafuerza::class.java)
+            startActivity(intent)
+        }
+
+        btnAltaIntensidad.setOnClickListener {
+            val intent = Intent(requireContext(), activity_altaintensidad::class.java)
+            startActivity(intent)
+        }
+        btnYoga.setOnClickListener {
+            val intent = Intent(requireContext(), activity_yoga::class.java)
+            startActivity(intent)
+        }
+
 
 
         return root
