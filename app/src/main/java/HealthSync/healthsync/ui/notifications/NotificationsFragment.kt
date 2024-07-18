0da6d7fdb485156a91da03441ea81dc7efcff989
Josue <Fimel505011@gmail.com>
2024-07-17 21:00:13 -0600

    Terminamos el diseño de estadisticas

--- app/src/main/java/HealthSync/healthsync/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/HealthSync/healthsync/ui/notifications/NotificationsFragment.kt
@@ -28,9 +28,8 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+
         notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
         }
         return root
     }
