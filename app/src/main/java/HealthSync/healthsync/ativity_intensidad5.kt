01775ace652a0f5170b68aacc12cc3380c927d30
ethan <20230006@ricaldone.edu.sv>
2024-09-02 22:23:00 -0600

    mañana sigo

--- app/src/main/java/HealthSync/healthsync/ativity_intensidad5.kt
+++ app/src/main/java/HealthSync/healthsync/ativity_intensidad5.kt
@@ -1,12 +1,22 @@
 package HealthSync.healthsync
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ativity_intensidad5 : AppCompatActivity() {
+
+    private lateinit var txtTimer: TextView
+    private lateinit var pauseButton: ImageView
+    private var countDownTimer: CountDownTimer? = null
+    private var timeRemaining: Long = 60000 // 1 min en milisegundos
+    private var isPaused: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +27,48 @@ class ativity_intensidad5 : AppCompatActivity() {
             insets
         }
         supportActionBar?.hide()
+
+        // Referencias a los elementos en el layout
+        txtTimer = findViewById(R.id.txt1minIn5)
+        pauseButton = findViewById(R.id.img1minIn5)
+
+        // Configurar el CountdownTimer
+        countDownTimer = createCountDownTimer(timeRemaining)
+
+        // Iniciar el temporizador
+        countDownTimer?.start()
+
+        // Configurar el botón de pausa
+        pauseButton.setOnClickListener {
+            if (isPaused) {
+                // Reanudar el temporizador
+                countDownTimer = createCountDownTimer(timeRemaining)
+                countDownTimer?.start()
+                isPaused = false
+            } else {
+                // Pausar el temporizador
+                countDownTimer?.cancel()
+                isPaused = true
+            }
+        }
+    }
+
+    private fun createCountDownTimer(timeInMillis: Long): CountDownTimer {
+        return object : CountDownTimer(timeInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeRemaining = millisUntilFinished
+                val secondsRemaining = millisUntilFinished / 1000
+                txtTimer.text =
+                    String.format("%02d:%02d", secondsRemaining / 60, secondsRemaining % 60)
+            }
+
+            override fun onFinish() {
+                txtTimer.text = "00:00"
+                // val intent = Intent(this@, ::class.java)
+                // startActivity(intent)
+                // finish()
+            }
+        }
 
     }
 }
