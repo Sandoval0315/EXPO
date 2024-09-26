1d6942b2e21fb33961bb138e95c69cd70f81950e
ethan <20230006@ricaldone.edu.sv>
2024-09-26 11:26:35 -0600

    messi

--- app/src/main/java/HealthSync/healthsync/activity_tiempovascular6.kt
+++ app/src/main/java/HealthSync/healthsync/activity_tiempovascular6.kt
@@ -6,6 +6,7 @@ import android.os.CountDownTimer
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -35,10 +36,6 @@ class activity_tiempovascular6 : AppCompatActivity() {
         //Nos manda al inicio de la rutina (Boton para atras)
         val regresaraInicio = findViewById<ImageView>(R.id.imgBackk)
 
-        regresaraInicio.setOnClickListener{
-            val intent = Intent(this, activity_rutina_cardiovascular::class.java)
-            startActivity(intent)
-        }
 
         // Referencias a los elementos en el layout
         txtTimer = findViewById(R.id.txt30segT6)
@@ -63,6 +60,27 @@ class activity_tiempovascular6 : AppCompatActivity() {
                 isPaused = true
             }
         }
+
+        regresaraInicio.setOnClickListener {
+            mostrarDialogoConfirmacion()
+        }
+    }
+
+    private fun mostrarDialogoConfirmacion() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("¿Regresar a la pantalla anterior?")
+        builder.setMessage("Tu tiempo de actividad no se guardará")
+        builder.setPositiveButton("Sí") { dialog, which ->
+            val intent = Intent(this, activity_rutina_cardiovascular::class.java)
+            startActivity(intent) // Inicia la actividad deseada
+            finish() // Cierra la actividad actual
+        }
+        builder.setNegativeButton("No") { dialog, which ->
+            dialog.dismiss() // Cierra el diálogo sin realizar acción
+        }
+        val dialog = builder.create()
+        dialog.show()
+
     }
 
     private fun createCountDownTimer(timeInMillis: Long): CountDownTimer {
